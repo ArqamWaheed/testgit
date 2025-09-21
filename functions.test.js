4ed5e3c585d0185a4ac14dde40c5b51a28c5b4dd
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-09-21 15:22:39 +0500

    Added the tests, now making them green

---
+++ functions.test.js
@@ -0,0 +1,46 @@
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./index.js";
+
+test('Capitalizes the first letter', () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test('Reverses a string', () => {
+  expect(reverseString("hello").toBe("olleh"));
+});
+
+test('Addition of two numbers', () => {
+  expect(calculator.add(10, 2)).toBe(12);
+});
+
+test('Subtraction of two numbers', () => {
+  expect(calculator.subtract(10, 2)).toBe(8);
+});
+
+test('Division of two numbers', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('Multiplication of two numbers', () => {
+  expect(calculator.multiply(10, 2)).toBe(20);
+});
+
+test('Cipher textWrapping', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('Cipher upperCase', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('Kh00r');
+});
+
+test('Cipher Punctuation', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('Analyze array function', () => {
+  expect(analyzeArray([1,8,3,4,2,6])).toBe({
+   average: 4,
+   min: 1,
+   max: 8,
+   length: 6
+  });
+})
---
+++ index.js
@@ -0,0 +1,38 @@
+function capitalize(input) { 
+    
+}
+
+function reverseString(input) {
+
+}
+
+const calculator = {
+    add: function(val1, val2) {
+        return val1 + val2;
+    },
+    subtract: function(val1, val2) {
+        return val1 - val2;
+    },
+    divide: function(val1, val2) {
+        return val1 / val2;
+    },    
+    multiply: function(val1, val2) {
+        return val1 * val2;
+    }
+}
+
+function caesarCipher(input, key) {
+
+}
+
+function analyzeArray(arrNum) {
+
+}
+
+export {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray,
+}
